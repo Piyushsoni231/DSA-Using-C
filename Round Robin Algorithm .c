2c055e66ca3b41312aecbfb74160fcf81ce5b02c
Piyushsoni231 <153917205+Piyushsoni231@users.noreply.github.com>
2024-10-02 23:41:52 +0530

    Update and rename experiment3.c to Round Robin Algorithm .c

--- Round Robin Algorithm .c
+++ Round Robin Algorithm .c
@@ -11,7 +11,7 @@ typedef struct process {
     int exeTime;
 } p;
 
-p processes[3] = {{1, 12}, {2, 14}, {3, 9}};  // Default processes with execution times
+p processes[3] = {{1, 12}, {2, 14}, {3, 9}};  
 
 void enqueue(int id) {
     if ((rear + 1) % size == front) {
@@ -35,7 +35,7 @@ int dequeue() {
     }
     int id = ProcessQ[front];
     if (front == rear) {
-        front = rear = -1;  // Queue is now empty
+        front = rear = -1;  
     } else {
         front = (front + 1) % size;
     }
@@ -43,32 +43,29 @@ int dequeue() {
 }
 
 void roundRobin() {
-    int time = 0;  // Time tracker
+    int time = 0;  
 
-    // Enqueue all processes initially
     for (int i = 0; i < size; i++) {
         enqueue(processes[i].id);
     }
 
-    while (front != -1) {  // Continue until the queue is empty
+    while (front != -1) {  
         int processId = dequeue();
 
-        // Find the corresponding process
         for (int i = 0; i < size; i++) {
             if (processes[i].id == processId) {
-                // Execute the process
+                
                 printf("Executing Process %d for %d seconds\n", processes[i].id, burstTime);
 
                 if (processes[i].exeTime <= burstTime) {
                     time += processes[i].exeTime;
                     printf("Process %d finished at time %d seconds\n", processes[i].id, time);
-                    processes[i].exeTime = 0;  // Mark as finished
+                    processes[i].exeTime = 0;  
                 } else {
                     time += burstTime;
-                    processes[i].exeTime -= burstTime;  // Decrease the remaining time
+                    processes[i].exeTime -= burstTime;  
                     printf("Process %d has %d seconds remaining\n", processes[i].id, processes[i].exeTime);
-
-                    // Re-enqueue the process if it is not finished
+                    
                     enqueue(processes[i].id);
                 }
                 break;
